50204deb705c1737564f61c1ec17497433a845f0
Smurfenokk <paradox1989ty@gmail.com>
2026-03-16 17:03:06 +0300

    Сделал задачи 1-35, позже сделаю оставшиеся 5

---
+++ Pribylnov_C/n24/n24.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main() {
+    int n, isPrime = 1;
+    scanf("%d", &n);
+    if (n < 2) isPrime = 0;
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+    printf("%s\n", isPrime ? "Prime" : "Not prime");
+    return 0;
+}
---
+++ Pribylnov_C/n4/n4.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    scanf("%d", &n);
+    for (int i = 5; i <= n; i += 5) {
+        sum += i;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
